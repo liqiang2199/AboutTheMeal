0e8fb49685f115f9c01b8c7f57013eb52afc74f3
Messcompetent <203361721@qq.com>
2018-02-27 10:47:43 +0800

    、、

--- app/src/main/java/com/atmeal/client/loginactivity/RegisterActivity.java
+++ app/src/main/java/com/atmeal/client/loginactivity/RegisterActivity.java
@@ -1,18 +1,11 @@
 package com.atmeal.client.loginactivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.os.Handler;
-import android.os.Message;
 import android.support.annotation.Nullable;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.atmeal.client.R;
 import com.atmeal.client.base.BaseFragmentActivity;
@@ -25,8 +18,6 @@ import com.atmeal.client.http.OkHttp_CallResponse;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import cn.smssdk.EventHandler;
-import cn.smssdk.SMSSDK;
 import okhttp3.Call;
 import okhttp3.Response;
 
@@ -39,102 +30,32 @@ public class RegisterActivity extends BaseFragmentActivity implements OkHttp_Cal
 
     private EditText register_name;
     private EditText register_password;
-    private EditText register_password_re,register_passwordyan;
+    private EditText register_password_re;
     private Button submit_but;
-    private TextView login_tip,yanzheng,queren;
-    private CountDownTimer timer;
-    EventHandler eventHandler;
-    int T=0;
+    private TextView login_tip;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         InitView();
-        yanzheng.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SMSSDK.getVerificationCode("86",register_name.getText().toString().trim());
-            }
-        });
-        timer = new CountDownTimer(60000, 1000) {
-
-            @Override
-            public void onTick(long millisUntilFinished) {
-                yanzheng.setEnabled(false);
-                yanzheng.setText((millisUntilFinished / 1000) + "秒后可重发");
-            }
-
-            @Override
-            public void onFinish() {
-                yanzheng.setEnabled(true);
-                yanzheng.setText("验证");
-            }
-        };
-
-        eventHandler = new EventHandler() {
-            public void afterEvent(int event, int result, Object data) {
-                if (data instanceof Throwable) {
-                    Throwable throwable = (Throwable)data;
-                    final String msg = throwable.getMessage();
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            //{"status":603,"detail":"请填写正确的手机号码"}
-                            if (!TextUtils.isEmpty(msg)){
-                                try {
-                                    JSONObject json = new JSONObject(msg);
-                                    if (json.has("detail")){
-                                        String msgtip = json.getString("detail");
-                                        Toast.makeText(RegisterActivity.this, msgtip, Toast.LENGTH_SHORT).show();
-                                    }
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-                    });
-                } else {
-                    if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE) {
-                        Log.v("yan","发送成功");
-                        timer.start();
-                    }else if (event==SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE){
-                        Log.v("yan","验证成功");
-                        Message msg=new Message();
-                        chandle.sendMessage(msg);
-                        T=200;
-                    }
-                }
-
-            }
-        };
-        SMSSDK.registerEventHandler(eventHandler);
     }
-    Handler chandle=new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            Toast.makeText(RegisterActivity.this,"验证通过",Toast.LENGTH_LONG).show();
-            queren.setEnabled(false);
-        }
-    };
+
     @Override
     public void InitView() {
         Init_TitleView();
         titleback.setText("注册");
-        yanzheng=findViewById(R.id.yanzheng);
+
         register_name = (EditText) findViewById(R.id.register_name);
         register_password = (EditText) findViewById(R.id.register_password);
         register_password_re = (EditText) findViewById(R.id.register_password_re);
-        register_passwordyan=findViewById(R.id.register_password_yan);
-        queren=findViewById(R.id.queren);
         submit_but = (Button) findViewById(R.id.submit_but);
         login_tip = (TextView) findViewById(R.id.login_tip);
         Span_Text(login_tip,"已有账号点此登录",4,8);
         submit_but.setText("注册");
-        submit_but.setEnabled(false);
+
         InitOnClick(submit_but);
         InitOnClick(login_tip);
-        InitOnClick(queren);
     }
     private void InitOnClick(View view){
         view.setOnClickListener(new View.OnClickListener() {
@@ -166,10 +87,6 @@ public class RegisterActivity extends BaseFragmentActivity implements OkHttp_Cal
                             StringCommon.String_Toast(context,"输入手机号有误");
                             return;
                         }
-                        if (T==0){
-                            StringCommon.String_Toast(context,"验证失败！");
-                            return;
-                        }
                         String loginurl = UrlCommon.Regist+"?phone="+name
                                 +"&password="+pass+"&registerType=1";
 
@@ -178,9 +95,6 @@ public class RegisterActivity extends BaseFragmentActivity implements OkHttp_Cal
                         break;
                     case R.id.login_tip:
                         finish();
-                        break;
-                    case R.id.queren:
-                        SMSSDK.submitVerificationCode("86",register_name.getText().toString().trim(),register_passwordyan.getText().toString().trim());
                         break;
                 }
             }
@@ -222,9 +136,5 @@ public class RegisterActivity extends BaseFragmentActivity implements OkHttp_Cal
     @Override
     public void OkHttp_CallToastShow(String msg, String tag) {
         StringCommon.String_Toast(context,msg);
-    }
-    protected void onDestroy() {
-        super.onDestroy();
-        SMSSDK.unregisterEventHandler(eventHandler);
     }
 }
